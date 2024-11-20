06ffc963ee143c466cf736d6807f219ec98dc8a8
tanjimN4 <tanjimahemed7@gmail.com>
2024-11-21 00:54:47 +0600

    fist

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from '../Home/Home';
+
+const Main = () => {
+    return (
+        <div className='max-w-screen-2xl mx-auto'>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Main;
